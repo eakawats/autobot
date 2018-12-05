11ca87ca2055976fdd5f7bcbff967352c1a47466
eakawats <38580850+eakawats@users.noreply.github.com>
2018-12-06 00:59:32 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php // callback.php
 require "vendor/autoload.php";
+#Namespace
+use \LINE\LINEBot\HttPClient\CurlHTTPClient;
+use \LINE\LINEBot;
+use \LINE\LINEBot\MessageBuilder;
+
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
-$access_token = 'UOeO9ZUwN4dTOpFqTnduO6vtbNy2+4+dpPbggF6b/fXpN5KQJunVpHLbbR8kSzOlKvbceXmLJREc2uSSkRFmvR2MfEkZnlG/vy4vxg9ptFjK2+NpPcKH4RkGV1TyMdHV1+gCjrsrs5CwGNjgwQuxKgdB04t89/1O/w1cDnyilFU=';
+$channel_token = 'UOeO9ZUwN4dTOpFqTnduO6vtbNy2+4+dpPbggF6b/fXpN5KQJunVpHLbbR8kSzOlKvbceXmLJREc2uSSkRFmvR2MfEkZnlG/vy4vxg9ptFjK2+NpPcKH4RkGV1TyMdHV1+gCjrsrs5CwGNjgwQuxKgdB04t89/1O/w1cDnyilFU=';
+$channel_secret = 'cffec1626cfe1853d2a472d2e6b83ce9';
 
 // Get POST body content
 $content = file_get_contents('php://input');
@@ -9,37 +15,21 @@ $content = file_get_contents('php://input');
 $events = json_decode($content, true);
 // Validate parsed JSON data
 if (!is_null($events['events'])) {
-// Loop through each event
-foreach ($events['events'] as $event) {
-// Reply only when message sent is in 'text' format
-if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-// Get text sent
-$text = $event['source']['userId'];
-// Get replyToken
-$replyToken = $event['replyToken'];
-// Build message to reply back
-$messages = [
-'type' => 'text',
-'text' => $text
-];
-// Make a POST Request to Messaging API to reply to sender
-$url = 'https://api.line.me/v2/bot/message/reply';
-$data = [
-'replyToken' => $replyToken,
-'messages' => [$messages],
-];
-$post = json_encode($data);
-$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-$result = curl_exec($ch);
-curl_close($ch);
-echo $result . "\r\n";
+  foreach ($events['events'] as $event) {
+    if($event['type']=='message'){
+      switch($event['type']=='message'){
+          case'text':
+          $replyToken = $event['replyToken'];
+          $respMessage ='Hello, your message is '.$event['message']['text'];
+          
+          $httpClient=newCurlHttpClient($channel_token);
+          $bot=newLINEBot($HttpClient, array('channelSelect'=> $channel_secret));
+          $textMessageBuilder = newTextMessageBuilder($respMesage);
+          $response=$bot->replyMessage($replyToken, $textMessageBuilder);
+     Breke;
+      }
+    }
+  }
 }
-}
-}
+        
 echo "OK";
